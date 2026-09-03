39ef588bae9f63a410b33fcdc8ecf80707643f93
1U25CS065 MOHANRAM K <mohanram012x@gmail.com>
2026-09-03 10:05:49 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,25 @@
+USE departmentDB;
+CREATE TABLE Departments(
+DepartmentID INT,
+DepartmentName VARCHAR(30)
+);
+INSERT INTO Departments VALUES
+(101,'Computer Science'),
+(102,'Mathematics'),
+(103,'Physics');
+CREATE TABLE students(
+StudentID INT,
+StudentName VARCHAR(20),
+DepartmentID INT
+);
+INSERT INTO students VALUES
+(1001,'Arun',101),
+(1002,'Divya',102),
+(1003,'Karthik',101),
+(1004,'Nisha',103);
+SELECT students.StudentName,
+Departments.DepartmentName
+FROM students
+INNER JOIN Departments
+ON students.DepartmentID = Departments.DepartmentID;
 
